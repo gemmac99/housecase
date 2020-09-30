bcedd978d3d1b1c1b12100b41a3ea1cb222a5301
gemmac99 <69032374+gemmac99@users.noreply.github.com>
2020-09-30 12:28:17 +0800

    modified:   server.js

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ const upload = multer({
   }
 })
 
-mongoose.connect("mongodb+srv://gem:<password>@cluster0.ihone.mongodb.net/<dbname>?retryWrites=true&w=majority", {
+mongoose.connect("mongodb+srv://gem:1234@cluster0.ihone.mongodb.net/<dbname>?retryWrites=true&w=majority", {
     useNewUrlParser: true
 })
 
