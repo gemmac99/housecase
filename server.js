cf9c038ff3bc6c984dfdb750c28b7387869f36e6
gemmac99 <69032374+gemmac99@users.noreply.github.com>
2020-09-30 14:43:11 +0800

    late

--- server.js
+++ server.js
@@ -297,6 +297,7 @@ app.get("/delete:_id",(req,res)=>{
     
 })
 
-app.listen(3000, function(){
-    console.log("Now listening to port 3000")
-})
+//app.listen(3000, function(){
+   // console.log("Now listening to port 3000")
+//})
+app.listen(process.env.PORT || 3000)
